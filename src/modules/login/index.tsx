57cb5f337e6ff09ba89a679cea59bf1b026255a1
yourgotopyromaniac <cipherx809@gmail.com>
2025-11-29 16:14:38 +0100

    feat: auth page image container

--- src/modules/login/index.tsx
+++ src/modules/login/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styles from "./login.module.scss";
+import pablo from "../../assets/img/pablo-sign-in.svg";
+import logo from "../../assets/img/logo.svg";
 
 export interface LoginUIProps {}
 
 const LoginUI: React.FC<LoginUIProps> = ({}) => {
   return (
-    <div>
-      <div>LoginUI</div>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img src={logo} className={styles.logo} alt="Lendsqr logo" />
+        <img src={pablo} alt="Login illustration" />
+      </div>
+      <div></div>
     </div>
   );
 };
